4636439cddddb00bde6e9edefffb6a1becf9cbb8
KKimura <19808712+k1mny@users.noreply.github.com>
2022-01-16 15:20:06 +0900

    kimnyだけ配置

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../theme/index'
+import '@fontsource/alfa-slab-one'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
---
+++ theme/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+  styles: {
+    global: {
+      'html, body': {
+        color: 'black',
+        lineHeight: 'tall',
+        height: 'full',
+        width: 'full',
+        bg: "#FBFBFF"
+      },
+    },
+  },
+  fonts: {
+    kimnyTitle: 'Alfa Slab One'
+  },
+  colors: {
+    kimnybg: "#FBFBFF"
+  }
+};
+
+export default styles
