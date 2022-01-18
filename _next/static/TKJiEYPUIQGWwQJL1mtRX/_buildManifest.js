136a72bdc44dd4851b99b730cda3377f82745d0f
k1mny <k1mny@users.noreply.github.com>
2022-01-18 11:52:26 +0000

    deploy: 833ebb91156006f532b9676646b4b6bf7d1c67b5

--- _next/static/TKJiEYPUIQGWwQJL1mtRX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-7efcd3a353d1d8de.js","static/chunks/609-22c4ec5258642583.js","static/chunks/pages/index-7e46a856318089d1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
