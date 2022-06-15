00a954ab66e9cc1d36aba9e8acdabef63fdba87b
k1mny <k1mny@users.noreply.github.com>
2022-06-15 11:07:26 +0000

    deploy: 3c4efa5589a3abfdf51d7ef242e470517221ec9a

--- _next/static/chunks/pages/fonts-4a5b31e1b3b93b51.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[526],{3808:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/fonts",function(){return u(9647)}])}},function(n){n.O(0,[774,888,179],(function(){return _=3808,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
