f45d1f15c879dc738226a639bb46753f25c6248b
k1mny <k1mny@users.noreply.github.com>
2022-06-15 11:14:46 +0000

    deploy: f5770b7f14041bf2cadacdd6c323c3c7750bbe7d

---
+++ _next/static/Cl2dh-JVhVplowvR2VNUD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-ab243316717ca7b2.js","static/chunks/825-c02af02d3e66fe24.js","static/chunks/pages/index-6706a4ee36ffbfc9.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
