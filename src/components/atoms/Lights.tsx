0e31d9d7b13e30a980e9e559d80dfd65e4905c91
KKimura <19808712+k1mny@users.noreply.github.com>
2022-08-06 00:29:02 +0900

    fix Lights

--- src/components/atoms/Lights.tsx
+++ src/components/atoms/Lights.tsx
@@ -1,22 +1,39 @@
+import * as THREE from 'three';
 import { SpotLight, useDepthBuffer, useScroll } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
 import { FC, useRef } from 'react';
 
-const Lights: FC = () => {
+interface LightsProps {
+  position: THREE.Vector3;
+}
+const Lights: FC<LightsProps> = ({ position }) => {
   const depthBuffer = useDepthBuffer({ size: 256 });
-  const { width, height } = useThree((state) => state.viewport);
+  const scroll = useScroll();
+  const ref = useRef<THREE.SpotLight>(null!);
+
+  useFrame((state, delta) => {
+    const r1 = scroll.range(0 / 4, 1 / 4);
+
+    ref.current.position.x = THREE.MathUtils.damp(
+      ref.current.position.x,
+      (r1 - 0.5) * position.x,
+      4,
+      delta,
+    );
+  });
 
   return (
     <>
       {/* Purple */}
       <SpotLight
+        ref={ref}
         penumbra={0.5}
         depthBuffer={depthBuffer}
-        position={[-width / 2, 10, 10]}
-        intensity={50}
-        angle={Math.PI / 2}
+        position={position}
+        intensity={150}
+        angle={Math.PI / 3}
         distance={100}
-        color='#f0f0f0'
+        color='#c1c2e2'
         castShadow
       />
     </>
--- src/components/templates/MirrorBox.tsx
+++ src/components/templates/MirrorBox.tsx
@@ -4,6 +4,7 @@ import { FC, useRef } from 'react';
 import MagicMirror from '../molecules/MagicMirror';
 import { Box, ScrollControls, useScroll } from '@react-three/drei';
 import Text from '../molecules/Text';
+import Lights from '../atoms/Lights';
 
 const MirrorBox: FC = () => {
   const ref = useRef<THREE.Group>(null!);
@@ -12,14 +13,15 @@ const MirrorBox: FC = () => {
   const size = width / 3;
 
   useFrame((state, delta) => {
-    const r1 = scroll.range(0 / 4, 1);
+    const r = scroll.range(0 / 4, 1);
+    const r1 = scroll.range(0 / 4, 1 / 4);
     const r2 = scroll.range(1 / 4, 2 / 4);
     const r3 = scroll.range(2 / 4, 3 / 4);
     const r4 = scroll.range(3 / 4, 4 / 4);
 
     ref.current.rotation.y = THREE.MathUtils.damp(
       ref.current.rotation.y,
-      ((Math.PI * 3) / 2) * r1,
+      ((Math.PI * 3) / 2) * r,
       4,
       delta,
     );
@@ -31,7 +33,8 @@ const MirrorBox: FC = () => {
         rotation={[0, 0, 0] as unknown as THREE.Euler}
         size={size}
       >
-        <color attach='background' args={['#ffffff']} />
+        <color attach='background' args={['#000000']} />
+        <Lights position={new THREE.Vector3(5, 10, 10)} />
         <Text
           position={new THREE.Vector3(0, 0, 0)}
           rotation={new THREE.Euler(0, 0, 0)}
@@ -41,11 +44,11 @@ const MirrorBox: FC = () => {
         </Text>
       </MagicMirror>
       <MagicMirror
-        position={[size / 2, 0, 0] as unknown as THREE.Vector3}
-        rotation={[0, Math.PI / 2, 0] as unknown as THREE.Euler}
+        position={[-size / 2, 0, 0] as unknown as THREE.Vector3}
+        rotation={[0, -Math.PI / 2, 0] as unknown as THREE.Euler}
         size={size}
       >
-        <color attach='background' args={['#0000ff']} />
+        <color attach='background' args={['#00ff00']} />
         <Box />
       </MagicMirror>
       <MagicMirror
@@ -57,11 +60,11 @@ const MirrorBox: FC = () => {
         <Box />
       </MagicMirror>
       <MagicMirror
-        position={[-size / 2, 0, 0] as unknown as THREE.Vector3}
-        rotation={[0, -Math.PI / 2, 0] as unknown as THREE.Euler}
+        position={[size / 2, 0, 0] as unknown as THREE.Vector3}
+        rotation={[0, Math.PI / 2, 0] as unknown as THREE.Euler}
         size={size}
       >
-        <color attach='background' args={['#00ff00']} />
+        <color attach='background' args={['#0000ff']} />
         <Box />
       </MagicMirror>
     </group>
