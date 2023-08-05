307ee8dffc5ea907585cb5c26b1390de824dce83
kimny WSL <kcozy0220@gmail.com>
2023-08-05 23:14:50 +0900

    写真を変更

--- collect.sh
+++ collect.sh
@@ -3,8 +3,8 @@
 # ディレクトリパス
 directory="./public/photos/"
 
-# 拡張子が.jpgのファイルを取得
-file_list=$(find "$directory" -type f -name "*.jpg")
+# 拡張子が.jpgのファイルを取得して辞書順にソート
+file_list=$(find "$directory" -type f -name "*.jpg" | sort -r)
 
 # ファイル名をダブルクォーテーションで囲み、角括弧で囲んでリスト形式で表示
 echo "["
